79b56bc6f9439d17b49da27ebbdfbe737a7dbf6c
MRemushev <mremushev@gmail.com>
2021-05-03 14:50:58 +0700

    Add Wallet.py

---
+++ wallet.py
@@ -0,0 +1,23 @@
+class Wallet:
+    def __init__ (self, curency, name = "Unknown"):
+        self.__money = 0.0
+        self.curency = curency
+        self.name = name
+
+    def info (self):
+        print (self.name[:-1] + " has " + str(self.__money) + " " + self.curency)
+
+
+    def upMoney(self, howmoney):
+        self.__money += howmoney
+        print("The operation was successful!")
+
+    def downMoney (self, howmoney):
+        if (0 <= self.__money - howmoney):
+            self.__money -= howmoney
+            print("The operation was successful!")
+        else:
+            print("Not enough funds, top up your account")
+
+    def __del__(self):
+        return "Wallet deleted"
